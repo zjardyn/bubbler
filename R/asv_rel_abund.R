79217211ef323af79406a8c797970e62dc87b51b
zjardyn <zlieraho@uwaterloo.ca>
2024-04-28 01:06:04 -0400

    renamed asv_rel_abund to rel_abund

--- R/asv_rel_abund.R
+++ R/asv_rel_abund.R
@@ -1,4 +1,4 @@
-asv_rel_abund <- function(phy) {
+rel_abund <- function(phy) {
 
     taxonomy <- phyloseq::tax_table(phy) %>%
         as.data.frame() %>%
@@ -26,46 +26,14 @@ asv_rel_abund <- function(phy) {
         tidyr::pivot_longer(taxon_lvls,
                             names_to = "level",
                             values_to = "taxon")
-
 }
 
 choose_taxa_level <- function(rel_abund, taxon_level = "Phylum") {
    rel_abund %>%
         dplyr::filter(level == taxon_level)
 }
+
 choose_samples <- function(rel_abund, smp_selection) {
     rel_abund %>%
         dplyr::filter(sample_id == paste(smp_selection, sep = " || "))
 }
-# load("C:/Users/zjard/OneDrive/Desktop/bubbler/data/physeq1.rda")
-# a <- asv_rel_abund(physeq1)
-# b <- choose_taxa_level(a, taxon_level = "Genus")
-# c <- choose_samples(b, c("Smp1", "Smp2", "Smp3", "Smp4"))
-#
-#
-# a %>%
-#     group_by(sample_id) %>%
-#     summarise(sum = sum(rel_abund))
-#
-# a %>%
-#     group_by(taxon) %>%
-#     summarise(sum = sum(rel_abund)) %>%
-#     arrange(desc(sum))
-#
-# b %>%
-#     group_by(sample_id) %>%
-#     summarise(sum = sum(rel_abund))
-#
-# b %>%
-#     group_by(taxon) %>%
-#     summarise(sum = sum(rel_abund)) %>%
-#     arrange(desc(sum))
-#
-# c %>%
-#     group_by(sample_id) %>%
-#     summarise(sum = sum(rel_abund))
-#
-# c %>%
-#     group_by(taxon) %>%
-#     summarise(sum = sum(rel_abund)) %>%
-#     arrange(desc(sum))
--- R/pipeline.R
+++ R/pipeline.R
@@ -1,12 +1,14 @@
-load("C:/Users/zjard/OneDrive/Desktop/bubbler/data/physeq1.rda")
-a <- rel_abund(physeq1)
-b <- choose_taxa_level(a, taxon_level = "Genus")
-c <- choose_samples(b, c("Smp1", "Smp2", "Smp3", "Smp4"))
-
-threshold <- choose_n_taxa(c, 8)
-
-# a %>%
-#     group_by(sample_id) %>%
+# load("C:/Users/zjard/OneDrive/Desktop/bubbler/data/physeq1.rda")
+# a <- rel_abund(physeq1)
+# b <- choose_taxa_level(a, taxon_level = "Genus")
+# c <- choose_samples(b, c("Smp1", "Smp2", "Smp3", "Smp4"))
+# #
+# threshold <- choose_n_taxa(c, 6)
+#
+# pool_taxon(c, threshold)
+# #
+# # a %>%
+# #     group_by(sample_id) %>%
 #     summarise(sum = sum(rel_abund))
 #
 # a %>%
