4eeb53cb9aa22b7eb2d902d58df7f7736201e1b4
zjardyn <zlieraho@uwaterloo.ca>
2024-04-28 17:57:32 -0400

    Added show_top_taxa, fixed choose_n_taxa to properly choose the number of distinct taxa when when n_taxa is too large.
    Added pool_taxon, which works with and without a variable.

--- R/threshold.R
+++ R/threshold.R
@@ -1,43 +1,62 @@
-choose_n_taxa <- function(rel_abund, n_taxa = 3) {
-    nrow <- rel_abund %>%
-    dplyr::group_by(taxon) %>%
-    dplyr::summarise(sum = sum(rel_abund)) %>%
-    nrow()
+show_top_taxa <- function(rel_abund) {
 
-    if(n_taxa > nrow)  {
-        n_taxa = nrow
+    rel_abund %>%
+    dplyr::group_by(taxon) %>%
+    dplyr::summarise(max = max(rel_abund)) %>%
+    dplyr::arrange(desc(max))
+}
+
+choose_n_taxa <- function(rel_abund, n_taxa = 3) {
+
+    unique_taxa <- rel_abund %>%
+        dplyr::pull(taxon) %>%
+        dplyr::n_distinct()
+
+    if(n_taxa > unique_taxa)  {
+        n_taxa <- unique_taxa
     }
 
     rel_abund %>%
     dplyr::group_by(taxon) %>%
-    dplyr::summarise(sum = sum(rel_abund)) %>%
-    dplyr::arrange(desc(sum)) %>%
+    dplyr::summarise(max = max(rel_abund)) %>%
+    dplyr::arrange(desc(max)) %>%
         dplyr::slice(n_taxa) %>%
-        dplyr::pull(sum) %>%
-        round(2)
+        dplyr::pull(max)
 }
 
 .pool_taxon_thresh <- function(rel_abund, threshold = 0.2) {
+
    rel_abund %>%
         dplyr::group_by(taxon) %>%
-        dplyr::summarise(pool = sum(rel_abund) < threshold,
+        dplyr::summarise(pool = max(rel_abund) < threshold,
                          mean = mean(rel_abund),
                          .groups = "drop")
 }
 
 .set_taxon_threshold <- function(rel_abund_pool, taxon) {
+
     rel_abund_pool %>%
-        dplyr::mutate(taxon = dplyr::if_else(pool, glue::glue("< {threshold}%"), taxon),
+        dplyr::mutate(taxon = dplyr::if_else(pool, glue::glue("< {round(threshold, 2)}%"), taxon),
                taxon = tidyr::replace_na(taxon, "Unclassified"))
 }
 
+pool_taxon <- function(rel_abund, threshold = 0.2, var = NULL) {
 
-pool_taxon <- function(rel_abund, threshold = 0.2){
     taxon_pool <- .pool_taxon_thresh(rel_abund, threshold)
-    dplyr::inner_join(rel_abund, taxon_pool, by = "taxon") %>%
-        .set_taxon_threshold(taxon) %>%
+    pooled <- dplyr::inner_join(rel_abund, taxon_pool, by = "taxon") %>%
+        .set_taxon_threshold(taxon)
+    if(is.null(var)){
+        pooled %>%
         dplyr::group_by(sample_id, taxon) %>%
         dplyr::reframe(rel_abund = sum(rel_abund),
                 mean = sum(mean)) %>%
         dplyr::distinct()
+    } else {
+        pooled %>%
+        dplyr::group_by(!!rlang::sym(var), sample_id, taxon) %>%
+        dplyr::reframe(rel_abund = sum(rel_abund),
+                mean = sum(mean),
+                !!var := !!rlang::sym(var)) %>%
+        dplyr::distinct()
+    }
 }
