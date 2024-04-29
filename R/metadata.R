6cd089b0ad40e156c549405a83498f22c76ba870
zjardyn <zlieraho@uwaterloo.ca>
2024-04-28 22:06:43 -0400

    doc

--- R/metadata.R
+++ R/metadata.R
@@ -1,3 +1,12 @@
+#' Grab metadata from a physeq object and format to tibble
+#'
+#' @param phy A phyloseq object containig sample_data
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' meta_data_phy(physeq1)
 meta_data_phy <- function(phy) {
     sample_id <- rownames(phyloseq::sample_data(phy))
     phyloseq::sample_data(phy) %>%
