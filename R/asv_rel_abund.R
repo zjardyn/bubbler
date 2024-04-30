2d423424f48cea84720157f3b614961efa3eba75
zjardyn <zlieraho@uwaterloo.ca>
2024-04-29 21:43:01 -0400

    choose family of fncs

--- R/asv_rel_abund.R
+++ R/asv_rel_abund.R
@@ -70,33 +70,3 @@ rel_abund_var <- function(phy, var) {
                             names_to = "level",
                             values_to = "taxon")
 }
-
-#' Filter a rel_abund table at a specific taxonomic level
-#'
-#' @param rel_abund A rel_abund table in tibble format.
-#' @param taxon_level A character vector of the taxonomic level.
-#'
-#' @return A tibble.
-#' @export
-#'
-#' @examples
-#' choose_taxa_level(rel_abund, "Phylum")
-choose_taxa_level <- function(rel_abund, taxon_level = "Phylum") {
-   rel_abund %>%
-        dplyr::filter(level == taxon_level)
-}
-
-#' Filter a rel_abund table for a selection of samples
-#'
-#' @param rel_abund A rel_abund table in tibble format.
-#' @param smp_selection A character vector of the selected samples.
-#'
-#' @return A tibble.
-#' @export
-#'
-#' @examples
-#' choose_samples(rel_abund, c("Smp1", "Smp2", "Smp3"))
-choose_samples <- function(rel_abund, smp_selection) {
-    rel_abund %>%
-        dplyr::filter(sample_id == paste(smp_selection, sep = " || "))
-}
---
+++ R/choose.R
@@ -0,0 +1,29 @@
+#' Filter a rel_abund table at a specific taxonomic level
+#'
+#' @param rel_abund A rel_abund table in tibble format.
+#' @param taxon_level A character vector of the taxonomic level.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' choose_taxa_level(rel_abund, "Phylum")
+choose_taxa_level <- function(rel_abund, taxon_level = "Phylum") {
+   rel_abund %>%
+        dplyr::filter(level == taxon_level)
+}
+
+#' Filter a rel_abund table for a selection of samples
+#'
+#' @param rel_abund A rel_abund table in tibble format.
+#' @param smp_selection A character vector of the selected samples.
+#'
+#' @return A tibble.
+#' @export
+#'
+#' @examples
+#' choose_samples(rel_abund, c("Smp1", "Smp2", "Smp3"))
+choose_samples <- function(rel_abund, smp_selection) {
+    rel_abund %>%
+        dplyr::filter(sample_id == paste(smp_selection, sep = " || "))
+}
