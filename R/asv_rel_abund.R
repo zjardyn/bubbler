974bf97a9fc11d2d5842e2c51b36c43998116734
zjardyn <zlieraho@uwaterloo.ca>
2024-05-02 22:17:42 -0400

    rel_abund by sample and variable works

--- R/asv_rel_abund.R
+++ R/asv_rel_abund.R
@@ -4,6 +4,7 @@ utils::globalVariables(c("sample_id", "count", ".", "asv", "level"))
 #'
 #' @param phy A phyloseq object containing an otu_table and tax_table.
 #' @param taxa_level A character value specifying the taxa level from Domain to species.
+#' @param var A character value of a variable to sum by.
 #' @param meta_data A logical value specifying if metadata should be included from the phyloseq object.
 #'
 #' @return A tibble.
@@ -11,19 +12,50 @@ utils::globalVariables(c("sample_id", "count", ".", "asv", "level"))
 #'
 #' @examples
 #' rel_abund(phy = physeq1)
-rel_abund <- function(phy, taxa_level = "Phylum", meta_data = FALSE) {
+rel_abund <- function(phy, taxa_level = "Phylum", var = NULL , meta_data = FALSE) {
 
     taxonomy <- taxa_data_phy(phy)
-    metadata <- meta_data_phy(phy)
+    if(meta_data == TRUE) {
+        metadata <- meta_data_phy(phy)
+    }
+
+   if(!is.null(var) & meta_data == TRUE) {
+       rel_abund <- asv_data_phy(phy)
+
+   rel_abund <- asv_data_phy(phy) %>%
+       tidyr::pivot_longer(-sample_id,
+                           names_to = "asv",
+                           values_to = "count") %>%
+       dplyr::inner_join(., metadata, by =  "sample_id") %>%
+       dplyr::group_by(!!rlang::sym(var)) %>%
+       dplyr::mutate(rel_abund = count/sum(count)) %>%
+       dplyr::ungroup() %>%
+       dplyr::select(-count) %>%
+       dplyr::inner_join(., taxonomy, by =  "asv")
+
+   } else if (!is.null(var) & meta_data == FALSE) {
+
+   rel_abund <- asv_data_phy(phy) %>%
+       tidyr::pivot_longer(-sample_id,
+                           names_to = "asv",
+                           values_to = "count") %>%
+       dplyr::group_by(!!rlang::sym(var)) %>%
+       dplyr::mutate(rel_abund = count/sum(count)) %>%
+       dplyr::ungroup() %>%
+       dplyr::select(-count) %>%
+       dplyr::inner_join(., taxonomy, by =  "asv")
+   }
+
+   if(is.null(var)) {
 
     rel_abund <- asv_data_phy(phy) %>%
         tidyr::pivot_longer(-sample_id,
                             names_to = "asv",
                             values_to = "count") %>%
         dplyr::mutate(rel_abund = count/sum(count)) %>%
-        dplyr::ungroup() %>%
         dplyr::select(-count) %>%
         dplyr::inner_join(., taxonomy, by =  "asv")
+   }
 
     taxa_lvls <- taxonomy %>%
         dplyr::select(-asv) %>%
