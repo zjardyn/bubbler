8c8fb944f2906fd5de36d8bb1a2c2e9b41817ec9
zjardyn <zlieraho@uwaterloo.ca>
2024-05-06 17:47:42 -0400

    readded data/

--- R/choose.R
+++ R/choose.R
@@ -1,14 +1,15 @@
 #' Subset samples from a rel_abund tibble and recompute rel_abund
 #'
 #' @param rel_abund_tab A tibble.
-#' @param smp_selection A character vector of sample selections.
+#' @param var A character vector of the variable to subset from.
+#' @param selection A character vector of elements to select.
 #'
 #' @return A tibble.
 #' @export
 #'
 #' @examples
 #' smp_selection <- c("Smp1", "Smp2", "Smp3", "Smp4", "Smp5")
-#' choose_samples_rel_abund(rel_abund_tab, smp_selection)
+#' subset_rel_abund(rel_abund_tab, var = "sample_id", selection = smp_selection)
 subset_rel_abund <- function(rel_abund_tab, var, selection) {
     rel_abund_tab %>%
             dplyr::filter(!!rlang::sym(var) %in% selection) %>%
