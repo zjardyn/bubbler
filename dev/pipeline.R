71dee7bc5777015be593f18567e2780ab0390e4b
zjardyn <zlieraho@uwaterloo.ca>
2024-06-03 14:47:57 +0200

    expert qiime2 example

--- dev/pipeline.R
+++ dev/pipeline.R
@@ -10,19 +10,66 @@ asv_qiime <- "inst/extdata/qiime/table-dada2.qza"
 taxa_qiime <- "inst/extdata/qiime/taxonomy.qza"
 metadata_qiime <- "inst/extdata/qiime/sample-metadata.tsv"
 
+
+# sample_id, asv, rel_abund, level, taxon
 q <- rel_abund_qiime(
     asv_qiime = asv_qiime,
     taxa_qiime = taxa_qiime,
     metadata_qiime = metadata_qiime,
-    taxa_level = "Genus",
-    var = "sample_id")
+    taxa_level = "Genus", )
 
-choose_n_taxa(q)
+q2 <- q %>%
+    pool_taxa(threshold = 0.005)
 
-q %>%
-    pool_taxa(threshold = 0.1) %>%
-    ggplot(aes(x = sample_id, y = rel_abund)) +
-    geom_bar(stat = "identity", aes(fill = taxon))
+q_no <- q2 %>%
+    filter(reported_antibiotic_usage == "No")
+
+q_yes <- q2 %>%
+    filter(reported_antibiotic_usage == "Yes")
+
+new_layer <- q %>%
+    group_by(body_site, reported_antibiotic_usage) %>%
+    summarise(sum = sum(rel_abund)) %>%
+    ungroup()
+
+no <- new_layer %>%
+    filter(reported_antibiotic_usage == "No")
+
+yes <- new_layer %>%
+    filter(reported_antibiotic_usage == "Yes")
+
+q2 %>%
+    ggplot(aes(x = body_site, y = rel_abund, fill = taxon)) +
+    geom_bar(stat = "identity", position = "fill") +
+    facet_wrap(~reported_antibiotic_usage)
+
+colors <- c(" " = "black")
+
+p1 <- q_no %>%
+    ggplot(aes(x = body_site, y = rel_abund, fill = taxon)) +
+    geom_bar(stat = "identity", position = "fill") +
+    geom_point(data = no, aes(x = body_site, y = sum, color = " "),
+               inherit.aes = FALSE) +
+    geom_line(data = no, aes(x = body_site, y = sum, group = 1, color = " "),
+               inherit.aes = FALSE) + scale_color_manual(values = colors) +
+    labs(x = "Body Site",
+         y = "Relative abundance within samples",
+         color = "Relative abundance between samples")
+
+p2 <- q_yes %>%
+    ggplot(aes(x = body_site, y = rel_abund, fill = taxon)) +
+    geom_bar(stat = "identity", position = "fill") +
+    geom_point(data = yes, aes(x = body_site, y = sum),
+               inherit.aes = FALSE) +
+    geom_line(data = yes, aes(x = body_site, y = sum, group = 1),
+               inherit.aes = FALSE) +
+    labs(x = "Body Site",
+         y = "Relative abundance within samples" )
+
+library(patchwork)
+
+p1 + p2 + plot_layout(guides = "collect", axes = "collect")
+
 
 
 # new dataset
