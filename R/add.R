122aa66e58a6290a97de7f5fc8ab3a860e49a6f1
zjardyn <zlieraho@uwaterloo.ca>
2024-06-14 13:05:26 -0400

    add data to table

---
+++ R/add.R
@@ -0,0 +1,10 @@
+#
+# add_taxa <- function(rel_abund_tab, taxa_data_type){
+#     if(missing(rel_abund_tab)){stop("Provide a rel_abund table.")}
+#     if(missing(taxa_data_type)){stop('Provide either "tsv", "phylo", "qiime"')}
+#
+#     if(taxa_data_type == "tsv"){
+#
+#     }
+#
+# }
