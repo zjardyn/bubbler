72b5e3a9435f327071e40610a0357d871a7ef626
zjardyn <zlieraho@uwaterloo.ca>
2024-07-22 16:46:19 -0400

    arrange_var fixed

--- R/arrange.R
+++ R/arrange.R
@@ -111,15 +111,28 @@ arrange_taxa <- function(rel_abund_tb, pooled = "top", order = "bottom") {
 #'
 #' @examples
 #' rel_abund_phy(phy = physeq, meta_data = TRUE) %>%
-#'     arrange_var(levels = "Location")
+#'     arrange_var(levels = "location")
 arrange_var <- function(rel_abund_tb, var = "sample_id", levels){
-   if(missing(rel_abund_tb)){stop("Provide a relative abundance table.")}
-   if(missing(levels)){stop("levels not provided.")}
+    if(missing(rel_abund_tb)){stop("Provide a relative abundance table.")}
+    if(missing(levels)){stop("levels not provided.")}
 
-   rel_abund_tb %>%
-   dplyr::mutate(!!rlang::sym(var) := as.factor(!!rlang::sym(var)),
-                 !!rlang::sym(var) := forcats::fct_relevel(!!rlang::sym(var), levels))
+    if(is.numeric(rel_abund_tb[[levels]])){
+
+        rel_abund_tb %>%
+            dplyr::mutate(!!rlang::sym(var) := as.factor(!!rlang::sym(var)),
+                          !!rlang::sym(var) := forcats::fct_reorder(!!rlang::sym(var), !!rlang::sym(levels)))
+
+    } else {
+
+       ord_levels <- rel_abund_tb %>%
+            dplyr::arrange(!!rlang::sym(levels)) %>% dplyr::distinct(!!rlang::sym(var)) %>% dplyr::pull(!!rlang::sym(var))
+
+        rel_abund_tb %>%
+                dplyr::mutate(!!rlang::sym(var) := factor(!!rlang::sym(var)),  # Convert to factor first
+                              !!rlang::sym(var) := forcats::fct_relevel(!!rlang::sym(var), ord_levels))
+    }
 }
+
 
 #' Arrange a variable by its grouped relative abundance
 #'
--- tests/testthat/test-arrange.R
+++ tests/testthat/test-arrange.R
@@ -1,5 +1,28 @@
 # devtools::load_all()
 #
+
+arrange_var0 <- function(rel_abund_tb, var = "sample_id", lvls){
+    if(missing(rel_abund_tb)){stop("Provide a relative abundance table.")}
+    if(missing(lvls)){stop("lvls not provided.")}
+
+    if(is.numeric(rel_abund_tb[[lvls]])){
+
+        rel_abund_tb %>%
+            dplyr::mutate(!!rlang::sym(var) := as.factor(!!rlang::sym(var)),
+                          !!rlang::sym(var) := forcats::fct_reorder(!!rlang::sym(var), !!rlang::sym(lvls)))
+
+    } else {
+
+       ord_lvls <- rel_abund_tb %>%
+            dplyr::arrange(!!rlang::sym(lvls)) %>% dplyr::distinct(!!rlang::sym(var)) %>% dplyr::pull(!!rlang::sym(var))
+
+        rel_abund_tb %>%
+                dplyr::mutate(!!rlang::sym(var) := factor(!!rlang::sym(var)),  # Convert to factor first
+                              !!rlang::sym(var) := forcats::fct_relevel(!!rlang::sym(var), ord_lvls))
+    }
+}
+
+
 test_that("arrange_sample_by_taxa works", {
     tb <- rel_abund_phy(phy = physeq)
     expect_no_error(arrange_sample_by_taxa(tb))
@@ -12,7 +35,7 @@ test_that("arrange_var_abund works", {
 
 test_that("arrange_var works", {
     tb <- rel_abund_phy(phy = physeq, meta_data = TRUE)
-    expect_no_error(arrange_var(tb, levels = "Location"))
+    expect_no_error(arrange_var(tb, levels = "location"))
 })
 
 test_that("arrange_taxa works", {
